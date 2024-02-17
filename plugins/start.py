2f1934a6a3ff41eb4bcf34bbece54c9ab65564bf
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-02-17 07:20:47 +0530

    start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,7 +93,7 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
                 )
-        await message.reply_photo(Config.START_PIC, caption=Txt.START_MSG.format(user.mention), reply_markup=button)
+        await message.reply_photo(config.START_PIC, caption=Txt.START_MSG.format(user.mention), reply_markup=button)
         return   
 
 #=====================================================================================##
