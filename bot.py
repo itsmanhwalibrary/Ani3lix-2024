463e406b869b39a895183b0fbfa8ca4f73c60b81
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2024-06-08 09:15:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,9 @@ import sys
 from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, CHANNEL_ID, PORT
+import pyrogram.utils
 
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 name ="""
 ░█████╗░░█████╗░██████╗░███████╗██╗░░██╗██████╗░░█████╗░████████╗███████╗
